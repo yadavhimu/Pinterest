170755e93e5bb5ec9c4906a0355177e6a8a1d533
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-16 20:57:20 +0530

    login logout page added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const plm = require("passport-local-mongoose")
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/pin");
 
