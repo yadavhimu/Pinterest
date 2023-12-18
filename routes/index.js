a13947ec75a16bda6d6b33224df8cde35ce6e9f7
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-12-18 11:25:11 +0530

    profile page work running

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,16 @@ const upload = require('./multer');
 passport.use(new localStrategy(userModel.authenticate()));
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {nav:false});
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register', {nav:false});
 });
 
 router.get('/profile', isLoggedIn, async function(req, res, next) {
   const user = await userModel.findOne({username: req.session.passport.user});
-  res.render('profile', {user});
+  res.render('profile', {user, nav:true});
 });
 
 router.post('/fileupload', isLoggedIn, upload.single("image"), async function(req, res, next) {
